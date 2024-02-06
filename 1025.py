7b49acac1fc0d64191c03950092e730ba49c43a4
SamRyong <zackinthebox@khu.ac.kr>
2024-02-06 17:30:54 +0900

    240206-2

--- 1025.py
+++ 1025.py
@@ -4,25 +4,27 @@ matrix = []
 
 result = -1
 
-def checksquare(a):
+def checksquare(num):
     temp = 0
     global result
-    num = int(a) 
-    print(num)
-    while (temp*temp < num):
+    while (temp*temp <= num):
         if (temp * temp == num):
             if (num > result):
                 result = num
         temp = temp + 1
 
-def seq(d,max):
-    a = 0
-    result = []
-    while (max >= a):
-        result.append(a)
-        a = a + d
-    
-    return result
+def travel(i,j,di,dj,reg):
+    global matrix
+
+    if ((i > N - 1) or (j > M - 1)):
+        return
+    if ((i < 0) or (j < 0)):
+        return
+    reg = reg + str(matrix[i][j])
+    checksquare(int(reg))
+    if (di == 0 and dj == 0):
+        return
+    travel(i+di,j+dj,di,dj,reg)
 
 
 for i in range(N):
@@ -33,17 +35,11 @@ for i in range(N):
 
 for i in range(N):
     for j in range(M):
-        if (i != 0 and j != 0):
-            num = 0
-            
-            rowseq = seq(i,N-1)
-            colseq = seq(j,M-1)
-
-
-            for k in range(min(len(rowseq),len(colseq))):    
-                num = str(num)
-                num = num + str(matrix[rowseq[k]][colseq[k]])
-            checksquare(num)
-             
+        for k in range(N):
+            for m in range(M):
+                travel(i,j,k,m,"")
+                travel(i,j,k,(-1 * m),"")
+                travel(i,j,(-1 * k),m,"")
+                travel(i,j,(-1 * k),(-1 * m),"")
 
 print(result)
